b16a215c923d984ef0292845a15c100ba4aa5f70
Abdirizak Mohamed <abdirizakm123@gmail.com>
2023-02-16 14:25:38 +0300

    assignment completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+  attr_accessor :length, :width, :height
+
+  def initialze(length, width, height)
+    @length = length
+    @width = width
+    @height = height
+  end
+
+  def kind
+
+    if [@length, @width, @height].any? {|size| size <= 0}
+    raise TriangleError, "Positive measurements required"
+    end
+
+    sides = [@length, @width, @height].sort
+
+    unless sides[0] + sides[1] > sides[2]
+      raise TriangleError, "Triangle ineaquality rule invalidates"
+    end
+
+
+    if @length == @width && @width == @height
+      :equilateral
+    elsif @length == @width || @length == @height || @width == @height
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
+
+
+
